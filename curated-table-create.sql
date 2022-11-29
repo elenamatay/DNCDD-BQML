9e9aa6d132b33ad56af39928c4411418a9337165
Elena Mata Yandiola <47299995+elenamatay@users.noreply.github.com>
2022-11-29 09:14:10 +0100

    Update curated-table-create.sql

--- curated-table-create.sql
+++ curated-table-create.sql
@@ -1,5 +1,5 @@
 # Create a curated -subset- table from the original NOAA GFS public dataset
-# This curated table will just include 3 geopoints or cities, and their avgerage temperature each day until 1st May 2022
+# This curated table will just include 3 geopoints or cities, and their avgerage temperature each day
 # WARNING: This query should process ~27.13 TB when run
 
 CREATE OR REPLACE TABLE `project.dataset.table` PARTITION BY day CLUSTER BY city  # Add your project.ds.table path here
