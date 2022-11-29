d452aa57c4cad20425b9af5886dcc7e2efad326a
Elena Mata Yandiola <47299995+elenamatay@users.noreply.github.com>
2022-11-29 09:29:53 +0100

    Update model-predict.sql

--- model-predict.sql
+++ model-predict.sql
@@ -1,8 +1,9 @@
-#With this query we will predict the first 150 datapoints (150 days) using our trained forecast model.
+# With this query we will predict the first 150 datapoints (150 days) using our trained forecast model.
 
 # If we changed the function "ML.FORECAST" for "ML.EXPLAIN_FORECAST", BQ would return a superset of the result of this query. 
 # With such superset we would receive more information on the forecast made by our model. 
-# E.g.  we would see that the -nonsense- weekly seasonality detected is negligible compared to the yearly one.
+# E.g.  we would see that the -nonsense- weekly seasonality detected is negligible (as opposed to the yearly one).
+
 
 SELECT 
   *
