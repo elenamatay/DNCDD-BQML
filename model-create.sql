33503d79796d31855ea660477cdc28e75c2556e5
Elena Mata Yandiola <47299995+elenamatay@users.noreply.github.com>
2022-11-29 09:19:42 +0100

    Update model-create.sql

--- model-create.sql
+++ model-create.sql
@@ -1,3 +1,6 @@
+# Create (includes training) forecasting model to predict the avg temperature in Berlin, Madrid and Milano
+# Model trained with daily data from 2016-07-10 to 2022-05-01; coming from NOAA's subset curated table created in the previous step
+
 CREATE MODEL `project.dataset.table` # Add your project.ds.table path here
 
  OPTIONS(MODEL_TYPE='ARIMA_PLUS',
