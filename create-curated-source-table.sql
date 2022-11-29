270bddb9b96d824238d6138a6687a2134af2ee3e
Elena Mata Yandiola <47299995+elenamatay@users.noreply.github.com>
2022-11-29 10:06:36 +0100

    Update create-curated-source-table.sql

--- create-curated-source-table.sql
+++ create-curated-source-table.sql
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Create a curated -subset- table from the original NOAA GFS public dataset, in order to better visualise data for training
 # This curated table will just include 3 geopoints or cities, and their avgerage temperature each day
 # WARNING: This query should process ~27.13 TB when run
