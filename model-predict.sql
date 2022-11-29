f3a0e499011a00739613e2ae3868e857a44ee628
Elena Mata Yandiola <47299995+elenamatay@users.noreply.github.com>
2022-11-29 08:58:45 +0100

    Update and rename model-predict to model-predict.sql

---
+++ model-predict.sql
@@ -0,0 +1,14 @@
+#With this query we will predict the first 150 datapoints (150 days) using our trained forecast model.
+
+# If we changed the function "ML.FORECAST" for "ML.EXPLAIN_FORECAST", BQ would return a superset of the result of this query. 
+# With such superset we would receive more information on the forecast made by our model. 
+# E.g.  we would see that the -nonsense- weekly seasonality detected is negligible compared to the yearly one.
+
+SELECT 
+  *
+
+FROM
+  ML.FORECAST(
+    MODEL `project.dataset.model`,  # Add your project.ds.table path here for the trained model (treated as a table in BQ)
+    STRUCT(150 AS HORIZON)
+  )
